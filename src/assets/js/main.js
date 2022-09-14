b8179f1b1a5f593fed23c2ca0faa4fa375d6c74f
mdmunnaahmed <msdmunna77@gmail.com>
2022-09-14 09:38:13 +0600

    done for checking -munna

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -202,12 +202,14 @@ $(".menu li a").each(function () {
 		$(this).addClass("active");
 	}
 });
+$(".sidebar li a").each(function () {
+	if ($(this).attr("href").indexOf(current) !== -1 && current != "") {
+		$(this).addClass("active");
+	}
+});
 
 if ($(".datepicker-here-top-orientation")) {
-	$(".datepicker-here-top-orientation").datepicker({
-		orientation: "bottom",
-		autoclose: true,
-	});
+	$(".datepicker-here-top-orientation").datepicker();
 }
 
 $(".sidebar-toggler").on("click", function () {
@@ -219,99 +221,3 @@ $(".overlay, .sidebar-close").on("click", function () {
 	$(".sidebar").removeClass("active");
 	$(".overlay").removeClass("overlay-color");
 });
-
-$("").on("click", function () {
-	$("#click-to-remove").this(".faq-main-wrapper").remove();
-});
-$("#click-to-remove").on("click", function () {
-	$("#in-progress").removeClass(".d-none");
-});
-
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
-
-function showTab(n) {
-	// This function will display the specified tab of the form...
-	var x = document.getElementsByClassName("tab");
-	x[n].style.display = "block";
-	//... and fix the Previous/Next buttons:
-	if (n == 0) {
-		document.getElementById("prevBtn").style.display = "none";
-	} else {
-		document.getElementById("prevBtn").style.display = "inline";
-	}
-	if (n == x.length - 1) {
-		document.getElementById("nextBtn").innerHTML = "Submit";
-	} else {
-		document.getElementById("nextBtn").innerHTML = "Next";
-	}
-	//... and run a function that will display the correct step indicator:
-	fixStepIndicator(n);
-}
-
-function nextPrev(n) {
-	// This function will figure out which tab to display
-	var x = document.getElementsByClassName("tab");
-	// Exit the function if any field in the current tab is invalid:
-	if (n == 1 && !validateForm()) return false;
-	// Hide the current tab:
-	x[currentTab].style.display = "none";
-	// Increase or decrease the current tab by 1:
-	currentTab = currentTab + n;
-	// if you have reached the end of the form...
-	if (currentTab >= x.length) {
-		// ... the form gets submitted:
-		document.getElementById("regForm").submit();
-		return false;
-	}
-	// Otherwise, display the correct tab:
-	showTab(currentTab);
-}
-
-function validateForm() {
-	// This function deals with validation of the form fields
-	var x,
-		y,
-		i,
-		valid = true;
-	x = document.getElementsByClassName("tab");
-	y = x[currentTab].getElementsByTagName("input");
-	// A loop that checks every input field in the current tab:
-	for (i = 0; i < y.length; i++) {
-		// If a field is empty...
-		if (y[i].value == "") {
-			// add an "invalid" class to the field:
-			y[i].className += " invalid";
-			// and set the current valid status to false
-			valid = false;
-		}
-	}
-	// If the valid status is true, mark the step as finished and valid:
-	if (valid) {
-		document.getElementsByClassName("step")[currentTab].className += " finish";
-	}
-	return valid; // return the valid status
-}
-
-function fixStepIndicator(n) {
-	// This function removes the "active" class of all steps...
-	var i,
-		x = document.getElementsByClassName("step");
-	for (i = 0; i < x.length; i++) {
-		x[i].className = x[i].className.replace(" active", "");
-	}
-	//... and adds the "active" class on the current step:
-	x[n].className += " active";
-}
-
-
-var progressBarOptions = {
-	startAngle: -1.55,
-	size: 200,
-	value: 0.75,
-	fill: {
-		color: "#ffa500",
-	},
-};
-
-$('.circlechart').circlechart(); // Initialization
